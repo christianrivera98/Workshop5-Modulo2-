ce86ef18160ea94ffbecefc26cbc165620e6e00a
Christian <christiancamiloriveralamadrid@gmail.com>
2023-08-12 14:10:02 -0500

    Agregando servicios

--- workshop5-mudulo2/src/App.jsx
+++ workshop5-mudulo2/src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Homepage from './pages/Homepage'
+import MyProfile from './pages/MyProfile'
+import Profiles from "./pages/Profiles"
+import Login from "./pages/Login"
+import Signin from "./pages/Signin"
+import PublicationDetails from "./pages/PublicationDetails"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />}  />
+        <Route path='/my-profile' element={ <MyProfile />} />
+        <Route path='/profiles' element={ <Profiles />} />
+        <Route path='/publication-details' element={ <PublicationDetails />} />
+        <Route path='/login' element={ <Login />} />
+        <Route path='/sing-in' element={ <Signin />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ workshop5-mudulo2/src/services/users.js
@@ -0,0 +1,51 @@
+
+    export const users = [
+      {
+        "id": 1,
+        "username": "leo_messi",
+        "name": "Leo Messi",
+        "email": "leomessi@example.com",
+        "avatar": "https://i0.wp.com/elplanetaurbano.com/wp-content/uploads/2022/12/messi-record-instagram-planeta-urbano-01.jpg?resize=1080%2C781&ssl=1",
+        "frontpage": "https://i.pinimg.com/736x/5e/61/7b/5e617ba27b0cbdd6d2424d02630b2a68.jpg",
+        "followers": [], //aqui guardan solo los ids
+        "following": [], //aqui guardan solo los ids
+        "posts": [] // aqui guardan los ids
+      },
+    
+      {
+        "id": 2,
+        "username": "karol_g",
+        "name": "Karol G",
+        "email": "karolg@example.com",
+        "avatar": "https://static01.nyt.com/images/2023/02/19/multimedia/19KAROL-G-02-gcwq/19KAROL-G-02-gcwq-superJumbo.jpg",
+        "frontpage": "https://eldiariony.com/wp-content/uploads/sites/2/2022/07/GettyImages-1392054162-2.jpg?w=4096",
+        "followers": [], //aqui guardan solo los ids
+        "following": [], //aqui guardan solo los ids
+        "posts": [] // aqui guardan los ids
+      },
+
+      {
+        "id": 3,
+        "username": "kendrick_lamar",
+        "name": "Kendrick Lamar",
+        "email": "kendrick@example.com",
+        "avatar": "https://ismorbo.com/wp-content/uploads/2023/05/aplasticplant.jpg",
+        "frontpage": "https://imagenes.elpais.com/resizer/2u-x0VPxsHQ1QREUPZWP3crRdks=/1960x1470/cloudfront-eu-central-1.images.arcpublishing.com/prisa/UPMBJXHLPRE67OW46T2ZIVSAHA.jpg",
+        "followers": [], //aqui guardan solo los ids
+        "following": [], //aqui guardan solo los ids
+        "posts": [] // aqui guardan los ids
+      },
+
+      {
+        "id": 4,
+        "username": "Beyonce_diva",
+        "name": "Beyoncé",
+        "email": "beyonce@example.com",
+        "avatar": "https://www.show.news/__export/1655592409697/sites/debate/img/2022/04/28/beyoncx_1_x1x_x1x.jpeg_1694538543.jpeg",
+        "frontpage": "https://images.hola.com/mx/imagenes/famosos/2022090444971/beyonce-tiffany-fotos-joyas-am/0-207-962/beyonce1-t.jpg?tx=w_680",
+        "followers": [], //aqui guardan solo los ids
+        "following": [], //aqui guardan solo los ids
+        "posts": [] // aqui guardan los ids
+      },
+
+    ]
