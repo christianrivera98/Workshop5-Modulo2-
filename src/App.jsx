a887c57785f292fa48e91845f6adc67182f1a934
Christian <christiancamiloriveralamadrid@gmail.com>
2023-08-25 15:20:08 -0500

    Agregando feedHome, perfil, logout y hacer publicaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Homepage from './pages/Homepage'
-import MyProfile from './pages/MyProfile'
-import Profiles from "./pages/Profiles"
-import Login from "./pages/Login"
-import Signin from "./pages/Signin"
-import PublicationDetails from "./pages/PublicationDetails"
 
-function App() {
+import { AppRouter } from "./router/AppRouter";
+import { AppTheme } from "./theme";
 
+const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Homepage />}  />
-        <Route path='/my-profile' element={ <MyProfile />} />
-        <Route path='/profiles' element={ <Profiles />} />
-        <Route path='/publication-details' element={ <PublicationDetails />} />
-        <Route path='/login' element={ <Login />} />
-        <Route path='/sing-in' element={ <Signin />} />
-      </Routes>
-    </BrowserRouter>
+    <AppTheme>
+      <AppRouter />
+    </AppTheme>
   )
 }
 
